f298e074c9a07c1456cc4dd95691f00c852c505e
idk <problemsolver@openmailbox.org>
2017-11-27 02:33:23 -0500

    rules for starting it

--- include.mk
+++ include.mk
@@ -1,3 +1,15 @@
 
-build:
+addon-syncthing-build:
 	docker build --force-rm -t hoardercache-syncthing .
+
+run-daemon:
+	docker run -d --rm \
+		-h apthoarder-syncthing \
+		-p 43842:8384
+		--restart=always \
+		--volume "$(cache_directory)":/var/cache/apt-cacher-ng \
+		--name hoardercache-syncthing \
+		-t base-apt-cache
+
+syncthing-web:
+	sr W 127.0.0.1:43842
