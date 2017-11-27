133f0145008ca032af47e8db08ce9f581db0a949
idk <problemsolver@openmailbox.org>
2017-11-27 02:41:22 -0500

    fixed name of target

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.6
 RUN apk update
 RUN apk add syncthing
-RUN adduser -h /home/sync/ -S -D sync sync
-USER sync
+RUN adduser -h /home/sync/ -S -D st st
+USER st
 CMD syncthing
--- include.mk
+++ include.mk
@@ -1,15 +1,15 @@
 
 addon-syncthing-build:
-	docker build --force-rm -t hoardercache-syncthing .
+	docker build --force-rm -t hoardercache-syncthing -f hoardercache-syncthing/Dockerfile .
 
 addon-syncthing-run-daemon:
-	docker run -d --rm \
+	docker run -d \
 		-h apthoarder-syncthing \
-		-p 43842:8384
+		-p 43842:8384 \
 		--restart=always \
 		--volume "$(cache_directory)":/var/cache/apt-cacher-ng \
 		--name hoardercache-syncthing \
-		-t base-apt-cache
+		-t hoardercache-syncthing
 
 syncthing-web:
 	sr W 127.0.0.1:43842
